eb58d8946ee260914950dd33a21be65837090474
Robert David Grant <robert.david.grant@gmail.com>
2016-11-02 17:31:19 -0500

    Add some users and groups and methods to list them.

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
+BASE_URI = '/grouper/api/v1.0'
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+
+users = [
+        {'username': 'alice',
+         'email': 'alice@q.bio',
+         'groups': ['wheel', 'developers'],
+         },
+        {'username': 'bob',
+         'email': 'bob@q.bio',
+         'groups': ['developers'],
+         },
+        {'username': 'charlie',
+         'email': 'charlie@q.bio',
+         'groups': ['marketing'],
+         },
+    ]
+
+groups = [
+        {'groupname': 'wheel',
+         'users': ['alice'],
+         },
+        {'groupname': 'developers',
+         'users': ['alice', 'bob'],
+         },
+        {'groupname': 'marketing',
+         'users': ['charlie'],
+         },
+        {'groupname': 'www',
+         'users': [],
+         },
+    ]
+
+
+@app.route('/'.join((BASE_URI, 'users')),
+           methods=['GET'])
+def get_users():
+    return jsonify({'users': users})
+
+
+@app.route('/'.join((BASE_URI, 'groups')),
+           methods=['GET'])
+def get_groups():
+    return jsonify({'groups': groups})
 
 
 if __name__ == '__main__':
