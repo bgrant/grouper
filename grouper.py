f7556bd0148d1ab5ca4e5784bf000c8af96f358a
Robert David Grant <robert.david.grant@gmail.com>
2016-11-12 11:29:37 -0600

    Redo with a database backend.
    Also use marshmallow for verification and serialization.
    
    Also abandon Flask-restful :)

--- grouper.py
+++ grouper.py
@@ -1,138 +1,268 @@
-from flask import Flask, abort, request
-from flask_restful import Api, Resource
-from flask_script import Manager
+"""
+Grouper is a Python web service for manages users and groups.
 
+It exposes a JSON/REST API and stores the user and group info in a database.
+See README.md for more details.
+"""
+
+import os
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from marshmallow import Schema, fields, ValidationError
+
+
+# Config
 
 API_URL = '/grouper/api/v1'
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+FALLBACK_DB = 'sqlite:///' + os.path.join(BASEDIR, 'data.sqlite')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL') or
+                                         FALLBACK_DB)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 manager = Manager(app)
-api = Api(app)
 
 
-# Initial Data
+# Models
 
-users = {}
-groups = {}
-
-
-# Users Resource
-
-
-class UserListAPI(Resource):
-
-    def get(self):
-        return {'users': list(users.values())}
-
-    def post(self):
-        if not request.json:
-            abort(400)
-        user = dict()
-
-        user['username'] = request.json.get('username')
-        user['email'] = request.json.get('email')
-        user['groups'] = []
-
-        if not all((user['username'], user['email'])):
-            abort(400)
-
-        users[user['username']] = user
-        return {'user': user}, 201
+user_groups = db.Table('user_groups',
+        db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+        db.Column('group_id', db.Integer, db.ForeignKey('groups.id')),
+        )
 
 
-api.add_resource(UserListAPI,
-                 '/'.join((API_URL, 'users')),
-                 endpoint='users')
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    groups = db.relationship('Group',
+                             secondary=user_groups,
+                             backref=db.backref('users', lazy='dynamic'),
+                             lazy='dynamic')
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 
-class UserAPI(Resource):
+# Schemas
 
-    def get(self, username):
-        try:
-            return {'user': users[username]}
-        except KeyError:
-            abort(404)
-
-    def put(self, username):
-        if username not in users:
-            abort(404)
-        if not request.json:
-            abort(400)
-
-        users[username]['email'] = request.json['email']
-
-        return {'user': users[username]}
-
-    def delete(self, username):
-        if username not in users:
-            abort(404)
-        del users[username]
-        return {'result': True}
+def must_not_be_blank(data):
+    if not data:
+        raise ValidationError('Data not provided.')
 
 
-api.add_resource(UserAPI,
-                 '/'.join((API_URL, 'users', '<string:username>')),
-                 endpoint='username')
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=must_not_be_blank)
+    email = fields.Str(required=True, validate=must_not_be_blank)
+    groups = fields.Nested('GroupSchema', many=True, only='id')
 
 
-# Groups Resource
+class GroupSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=must_not_be_blank)
+    users = fields.Nested('UserSchema', many=True, only='id')
 
 
-class GroupListAPI(Resource):
-
-    def get(self):
-        return {'groups': list(groups.values())}
-
-    def post(self):
-        if not request.json:
-            abort(400)
-        group = dict()
-
-        group['groupname'] = request.json.get('groupname')
-        group['users'] = request.json.get('users')
-
-        if not group['groupname']:
-            abort(400)
-
-        groups[group['groupname']] = group
-        return {'group': group}, 201
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+group_schema = GroupSchema()
+groups_schema = GroupSchema(many=True)
 
 
-api.add_resource(GroupListAPI,
-                 '/'.join((API_URL, 'groups')),
-                 endpoint='groups')
+# API
 
 
-class GroupAPI(Resource):
-
-    def get(self, groupname):
-        try:
-            return {'group': groups[groupname]}
-        except KeyError:
-            abort(404)
-
-    def delete(self, groupname):
-        if groupname not in groups:
-            abort(404)
-        del groups[groupname]
-        return {'result': True}
-
-    def put(self, groupname):
-        if groupname not in groups:
-            abort(404)
-        if not request.json:
-            abort(400)
-
-        groups[groupname]['users'] = request.json['users']
-
-        return {'result': True}
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'message': 'Not found'}), 404)
 
 
-api.add_resource(GroupAPI,
-                '/'.join((API_URL, 'groups', '<string:groupname>')),
-                 endpoint='groupname')
+## User Resources
+
+
+@app.route(API_URL + '/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    result = users_schema.dump(users)
+    return jsonify({'users': result.data}), 200
+
+@app.route(API_URL + '/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"message": "User could not be found."}), 404
+    else:
+        user_result = user_schema.dump(user)
+        return jsonify({'user': user_result.data}), 200
+
+@app.route(API_URL + '/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"message": "User could not be found."}), 404
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted.'}), 200
+
+@app.route(API_URL + '/users', methods=['POST'])
+def add_user():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'message': "No input data provided."}), 400
+
+    # Validate and deserialize input
+    data, errors = user_schema.load(json_data)
+    if errors:
+        return jsonify(errors), 422
+
+    name = data['name']
+    if User.query.filter_by(name=name).count() != 0:
+        return jsonify({'message': "User exists."}), 409
+
+    email = data['email']
+    groups = data['groups']
+
+    # Create a new User
+    user = User(name=name, email=email, groups=groups)
+    db.session.add(user)
+    db.session.commit()
+
+    # Return
+    result = user_schema.dump(User.query.get(user.id))
+    return jsonify({'message': 'User added.',
+                    'user': result.data}), 201
+
+@app.route(API_URL + '/users/<int:user_id>', methods=['PUT'])
+def modify_user(user_id):
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'message': "No input data provided."}), 400
+
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"message": "User could not be found."}), 400
+
+    # Validate and deserialize input
+    name = json_data.get('name', user.name)
+    email = json_data.get('email', user.email)
+    groups = json_data.get('groups', user.groups).all()
+
+    data, errors = user_schema.load(
+            dict(name=name, email=email, groups=groups))
+    if errors:
+        return jsonify(errors), 422
+
+    # Modify the user
+    user.name = data['name']
+    user.email = data['email']
+    user.groups = data['groups']
+
+    db.session.add(user)
+    db.session.commit()
+
+    # Return
+    result = user_schema.dump(User.query.get(user.id))
+    return jsonify({'message': 'User added.',
+                    'user': result.data}), 200
+
+
+## Group Resources
+
+
+@app.route(API_URL + '/groups', methods=['GET'])
+def get_groups():
+    groups = Group.query.all()
+    result = groups_schema.dump(groups)
+    return jsonify({'groups': result.data}), 200
+
+@app.route(API_URL + '/groups/<int:group_id>', methods=['GET'])
+def get_group(group_id):
+    group = Group.query.get(group_id)
+    if group is None:
+        return jsonify({"message": "Group could not be found."}), 404
+    else:
+        group_result = group_schema.dump(group)
+        return jsonify({'group': group_result.data}), 200
+
+@app.route(API_URL + '/groups/<int:group_id>', methods=['DELETE'])
+def delete_group(group_id):
+    group = Group.query.get(group_id)
+    if group is None:
+        return jsonify({"message": "Group could not be found."}), 404
+    else:
+        db.session.delete(group)
+        db.session.commit()
+        return jsonify({'message': 'Group deleted.'}), 200
+
+@app.route(API_URL + '/groups', methods=['POST'])
+def add_group():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'message': "No input data provided."}), 400
+
+    # Validate and deserialize input
+    data, errors = group_schema.load(json_data)
+    if errors:
+        return jsonify(errors), 422
+
+    name = data['name']
+    if Group.query.filter_by(name=name).count() != 0:
+        return jsonify({'message': "Group exists."}), 409
+
+    users = data['users']
+
+    # Create a new Group
+    group = Group(name=name, users=users)
+    db.session.add(group)
+    db.session.commit()
+
+    # Return
+    result = group_schema.dump(Group.query.get(group.id))
+    return jsonify({'message': 'Group added.',
+                    'group': result.data}), 201
+
+@app.route(API_URL + '/groups/<int:group_id>', methods=['PUT'])
+def modify_group(group_id):
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'message': "No input data provided."}), 400
+
+    group = Group.query.get(group_id)
+    if group is None:
+        return jsonify({"message": "Group could not be found."}), 400
+
+    # Validate and deserialize input
+    name = json_data.get('name', group.name)
+    users = json_data.get('users', group.users).all()
+
+    data, errors = group_schema.load(
+            dict(name=name, users=users))
+    if errors:
+        return jsonify(errors), 422
+
+    # Modify the group
+    group.name = data['name']
+    group.users = data['users']
+
+    db.session.add(group)
+    db.session.commit()
+
+    # Return
+    result = group_schema.dump(Group.query.get(group.id))
+    return jsonify({'message': 'Group added.',
+                    'group': result.data}), 200
 
 
 if __name__ == '__main__':
+    db.create_all()
     manager.run()
--- test_grouper.py
+++ test_grouper.py
@@ -1,48 +1,16 @@
+"""
+System tests for grouper, to be run with a test runner like `pytest`.
+
+These tests assume a running server.
+"""
+
 from uuid import uuid4
 import requests
+from grouper import API_URL
 
 
 URL_BASE = 'http://localhost:5000'
-API_URL = 'grouper/api/v1'
-URL = '/'.join((URL_BASE, API_URL))
-
-
-users = {
-    'alice':
-        {'username': 'alice',
-         'email': 'alice@q.bio',
-         'groups': ['wheel', 'developers'],
-         },
-    'bob':
-        {'username': 'bob',
-         'email': 'bob@q.bio',
-         'groups': ['developers'],
-         },
-    'charlie':
-        {'username': 'charlie',
-         'email': 'charlie@q.bio',
-         'groups': ['marketing'],
-         },
-    }
-
-groups = {
-    'wheel':
-        {'groupname': 'wheel',
-         'users': ['alice'],
-         },
-    'developers':
-        {'groupname': 'developers',
-         'users': ['alice', 'bob'],
-         },
-    'marketing':
-        {'groupname': 'marketing',
-         'users': ['charlie'],
-         },
-    'www':
-        {'groupname': 'www',
-         'users': [],
-         },
-    }
+URL = URL_BASE + API_URL
 
 
 def random_name(prefix='user'):
@@ -55,72 +23,83 @@ def random_email():
 def test_get_users():
     r = requests.get('/'.join((URL, 'users')))
     assert r.status_code == 200
-    assert list(r.json().keys())[0] == 'users'
+    assert 'users' in set(r.json().keys())
 
 
 def test_add_update_delete_user():
-    username = random_name()
+    name = random_name()
     email = random_email()
-    data = dict(username=username,
+    data = dict(name=name,
                 email=email,
                 groups=[])
-    updated_data = dict(username=username,
+    updated_data = dict(name=name,
                         email='foo@bar.com',
                         groups=[])
 
     r = requests.post('/'.join((URL, 'users')), json=data)
+    uid = r.json()['user']['id']
+    uid = str(uid)
     assert r.status_code == 201
 
-    r = requests.get('/'.join((URL, 'users', username)))
+    r = requests.get('/'.join((URL, 'users', uid)))
     assert r.status_code == 200
-    assert r.json() == {'user': data}
+    result = r.json()
+    del result['user']['id']
+    assert result['user'] == data
 
-    r = requests.put('/'.join((URL, 'users', username)),
+    r = requests.put('/'.join((URL, 'users', uid)),
                      json=dict(email='foo@bar.com'))
     assert r.status_code == 200
 
-    r = requests.get('/'.join((URL, 'users', username)))
+    r = requests.get('/'.join((URL, 'users', uid)))
     assert r.status_code == 200
-    assert r.json() == {'user': updated_data}
+    result = r.json()
+    del result['user']['id']
+    assert result['user'] == updated_data
 
-    r = requests.delete('/'.join((URL, 'users', username)))
+    r = requests.delete('/'.join((URL, 'users', uid)))
     assert r.status_code == 200
 
-    r = requests.get('/'.join((URL, 'users', username)))
+    r = requests.get('/'.join((URL, 'users', uid)))
     assert r.status_code == 404
 
 
 def test_get_groups():
     r = requests.get('/'.join((URL, 'groups')))
     assert r.status_code == 200
-    assert list(r.json().keys())[0] == 'groups'
+    assert 'groups' in set(r.json().keys())
 
 
 def test_add_update_delete_group():
-    groupname = random_name('group')
-    data = dict(groupname=groupname,
+    name = random_name()
+    data = dict(name=name,
                 users=[])
-    updated_data = dict(groupname=groupname,
-                        users=['foo', 'bar'])
-
+    updated_data = dict(name='foo',
+                        users=[])
 
     r = requests.post('/'.join((URL, 'groups')), json=data)
+    uid = r.json()['group']['id']
+    uid = str(uid)
     assert r.status_code == 201
 
-    r = requests.get('/'.join((URL, 'groups', groupname)))
+    r = requests.get('/'.join((URL, 'groups', uid)))
     assert r.status_code == 200
-    assert r.json() == {'group': data}
+    result = r.json()
+    del result['group']['id']
+    assert result['group'] == data
 
-    r = requests.put('/'.join((URL, 'groups', groupname)),
-                     json=dict(users=['foo', 'bar']))
-    assert r.status_code == 200
-
-    r = requests.get('/'.join((URL, 'groups', groupname)))
-    assert r.status_code == 200
-    assert r.json() == {'group': updated_data}
-
-    r = requests.delete('/'.join((URL, 'groups', groupname)))
+    r = requests.put('/'.join((URL, 'groups', uid)),
+                     json=dict(name='foo'))
     assert r.status_code == 200
 
-    r = requests.get('/'.join((URL, 'groups', groupname)))
+    r = requests.get('/'.join((URL, 'groups', uid)))
+    assert r.status_code == 200
+    result = r.json()
+    del result['group']['id']
+    assert result['group'] == updated_data
+
+    r = requests.delete('/'.join((URL, 'groups', uid)))
+    assert r.status_code == 200
+
+    r = requests.get('/'.join((URL, 'groups', uid)))
     assert r.status_code == 404
